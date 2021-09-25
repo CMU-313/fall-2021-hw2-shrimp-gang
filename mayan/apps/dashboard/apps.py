3b7380a729e252d0b8ae7b0ae84900a3455aa2ae
Fernanda <fmolinag@andrew.cmu.edu>
2021-09-25 18:33:48 -0400

    Adding a new tab to the sidebar of the menu

--- mayan/apps/dashboard/apps.py
+++ mayan/apps/dashboard/apps.py
@@ -1,7 +1,17 @@
 from django.utils.translation import ugettext_lazy as _
 
 from mayan.apps.common.apps import MayanAppConfig
+from mayan.apps.common.menus import (
+    menu_facet, menu_list_facet, menu_main, menu_object, menu_related,
+    menu_secondary, menu_setup, menu_tools
+)
+from .links import (
+    link_document_dashboard_list
+)
 
+from .models import (
+    Dashboard
+)
 
 class DashboardApp(MayanAppConfig):
     app_namespace = 'dashboard'
@@ -10,3 +20,7 @@ class DashboardApp(MayanAppConfig):
     has_tests = False
     name = 'mayan.apps.dashboard'
     verbose_name = _('Dashboard')
+    menu_facet.bind_links(
+        links=(link_document_dashboard_list,), sources=(Dashboard,)
+        )
+    
