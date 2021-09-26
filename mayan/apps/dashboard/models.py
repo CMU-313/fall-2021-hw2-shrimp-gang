1bf96d3b8b1d83963d68fafa743870404f86af5e
root <root@DESKTOP-A2F2RAE.localdomain>
2021-09-26 19:14:17 -0400

    yeet

---
+++ mayan/apps/dashboard/models.py
@@ -0,0 +1,50 @@
+
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.db.models.deletion import CASCADE
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+
+
+## https://docs.djangoproject.com/en/3.2/topics/db/models/
+
+class Candidate(models.Model):
+    gpa = models.DecimalField(
+    max_digits=3, decimal_places=2, help_text=_('GPA of the candidate'))
+
+    undergrad_uni = models.CharField(
+    max_length=255, help_text=_('Undergraduate University of the candidate.'),
+    verbose_name=_('undergrad_uni'))
+
+    major = models.CharField(
+    max_length=255, help_text=_('Undergraduate Major of the candidate.'),
+    verbose_name=_('major'))
+
+class Reviewer(models.Model):
+    name = models.CharField(
+    max_length=255, help_text=_('Name of the reviewer.'),
+    verbose_name=_('Name'))
+    
+    total_rating = models.PositiveSmallIntegerField(
+    help_text=_('Overall Rating of candidate.'),
+    verbose_name=_('Overall Rating'))
+
+    letters_of_rec = models.PositiveSmallIntegerField(
+    help_text=_('Rating of Letters of Recommendation of candidate.'),
+    verbose_name=_('Letters of Recommendation Rating'))
+
+    interview = models.PositiveSmallIntegerField(
+    help_text=_('Interview Rating of candidate.'),
+    verbose_name=_('Interview Rating'))
+
+    essay = models.PositiveSmallIntegerField(
+    help_text=_('Essay Rating of candidate.'),
+    verbose_name=_('Essay Rating'))
+
+    extracurriculars = models.PositiveSmallIntegerField(
+    help_text=_('Extracurriculars Rating of candidate.'),
+    verbose_name=_('Extracurriculars Rating'))
+
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
--- mayan/apps/dashboard/views.py
+++ mayan/apps/dashboard/views.py
@@ -1,24 +1,36 @@
-from django.template import RequestContext
-from django.urls import reverse_lazy
-from django.utils.translation import ugettext_lazy as _
+# from django.template import RequestContext
+# from django.urls import reverse_lazy
+# from django.utils.translation import ugettext_lazy as _
 
-from mayan.apps.views.generics import ( SingleObjectListView )
-from .icons import icon_dashboard_link_icon
-from .links import (
-    link_document_dashboard_list
-)
+# from mayan.apps.views.generics import ( SingleObjectListView )
+# from .icons import icon_dashboard_link_icon
+# from .links import (
+#     link_document_dashboard_list
+# )
 
-class DashboardView(SingleObjectListView):
+# # class DashboardView(SingleObjectListView):
 
-    def get_extra_context(self):
-        return {
-            'hide_link': True,
-            'hide_object': True,
-            'title': _('Dashboard'),
-            'no_results_icon': icon_dashboard_link_icon,
-            'no_results_main_link': link_document_dashboard_list.resolve(
-                context=RequestContext(request=self.request)
-            ),
-            'no_results_text': _('...'),
-            'no_results_title': _('No dashboard available'),
-        } 
+# #     def get_extra_context(self):
+# #         return {
+# #             'hide_link': True,
+# #             'hide_object': True,
+# #             'title': _('Dashboard'),
+# #             'no_results_icon': icon_dashboard_link_icon,
+# #             'no_results_main_link': link_document_dashboard_list.resolve(
+# #                 context=RequestContext(request=self.request)
+# #             ),
+# #             'no_results_text': _('...'),
+# #             'no_results_title': _('No dashboard available'),
+# #         } 
+
+# class DashboardView(TemplateView):
+#     def source_queryset():
+#         return self.external_object.get_descendants()
+
+from django.http import HttpResponse
+from django.views import View
+
+class DashboardView(View):
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello, World!')
